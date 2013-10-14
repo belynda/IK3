33c7c1d50dc54d122ea25244cc96eda5f6798525
Belynda <Belynda@Belynda-PC>
2013-10-14 16:30:20 +0200

    Classe de la patrie programation (alpha )

--- Projet IK3/src/Fenetre.java
+++ Projet IK3/src/Fenetre.java
@@ -7,38 +7,50 @@ import javax.swing.JPanel;
 
 public class Fenetre extends JFrame {
 	
+	
 //TODO constructeur  de la fenetre 
 	  Fenetre(){
 		
-		//TODO modifier le totre de la fenetre 
+		//TODO modifier le titre de la fenetre 
 		setTitle("DenkiBlocks"); 
 		
 		//TODO  modifier la taille 
-		setSize(400,300);
+		setSize(800,500);
 		
 		//TODO click sur la croix entraine la fermeture de la fenetre 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//TODO centrer la fenetre par rapport à l'ecran 
 		 setLocationRelativeTo(null);
+		  
+		 //TODO FIGER la taille de la fenetre 
+		 this.setResizable(false);
 		
 		// TODO affichega de la fenetre 
 		setVisible(true);
 		
 		// TODO instanciation d'un objet JPanel 
 		JPanel pan = new JPanel(); 
-	
+		 
 		
 		// TODO definition de la couleur du fond 
-		pan.setBackground(Color.BLUE);
+       // pan.setBackground(Color.BLUE);
 		
 		//TODO on previent notre JFrame que notre JPanel sera son content pane 
 		this.setContentPane(pan); 
+		
+		this.setContentPane(new Panneau());
+		
+		
 		this.setVisible(true);
-	    
 		
 		
-		 this.setContentPane(new Panneau()); 
+		 
+	  }
+		 
+	      
+		 
+		 
 		
 		
 		
@@ -50,4 +62,4 @@ public class Fenetre extends JFrame {
 	
 
 
-}
+
--- Projet IK3/src/Panneau.java
+++ Projet IK3/src/Panneau.java
@@ -1,29 +1,57 @@
 import java.awt.Graphics;
+
 import javax.swing.JPanel; 
 import javax.imageio.ImageIO;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.awt.Color;
+
 
 
 
 public class Panneau  extends JPanel{
 	
+	
 	 public void paintComponent(Graphics g){
-		    // TODO Vous verrez cette phrase chaque fois que la méthode sera invoquée
-		    System.out.println("Je suis exécutée !"); 
-		  //  int x1 = this.getWidth()/4;
-		  //  int y1 = this.getHeight()/4;                      
-		  //  g.fillOval(x1, y1, this.getWidth()/2, this.getHeight()/2);	
+		  
+		   
 		    
-		    try {
-		        Image img = ImageIO.read(new File("src/images/image.png")); 
-		        g.drawImage(img, 0, 0, this);
-		        //Pour une image de fond
-		        g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-		      } catch (IOException e) {
-		        e.printStackTrace();
-		      }                
-		                                   }  
+		 try {
+			 
+			 //TODO importer l'image de fond 
+		      Image img = ImageIO.read(new File("src/images/foret.jpg"));
+		      g.drawImage(img, 0, 0, this);
+		      //Pour une image de fond
+		      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		      
+		// JPanel[][] plateau ; 
+		// plateau= new JPanel [10][10]; 
+		      
+		      
+		    
+		 
+		 
+		 
+		     
+		      
+		      
+		      
+		      
+		      
+		      
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }             
+		        
+		        // TODO Vous verrez cette phrase chaque fois que la méthode sera invoquée
+			    System.out.println("Je suis exécutée !"); 
+			    
+		  
+			
+	 }
+			  
+			 
+		
+	 }
    
-}
