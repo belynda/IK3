39b03e6cf8a69e99d46d07c2acf0ae960d4ac0db
Belynda <Belynda@Belynda-PC>
2013-10-14 19:32:13 +0200

    programmation du denkiBlocks

--- Projet IK3/src/Panneau.java
+++ Projet IK3/src/Panneau.java
@@ -1,11 +1,12 @@
+import java.awt.Color;
 import java.awt.Graphics;
-
-import javax.swing.JPanel; 
-import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-import java.awt.Color;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 
 
@@ -14,20 +15,53 @@ public class Panneau  extends JPanel{
 	
 	
 	 public void paintComponent(Graphics g){
-		  
-		   
+		  super.paintComponent(g);
+		  Graphics2D g2 = (Graphics2D)g;
 		    
 		 try {
 			 
 			 //TODO importer l'image de fond 
 		      Image img = ImageIO.read(new File("src/images/foret.jpg"));
-		      g.drawImage(img, 0, 0, this);
+		     // g.drawImage(img, 0, 0, this);
 		      //Pour une image de fond
-		      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		      g2.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 		      
 		// JPanel[][] plateau ; 
 		// plateau= new JPanel [10][10]; 
 		      
+		      //TODO Importer l'image d'une brique 
+		      
+		      Image img2 = ImageIO.read(new File("src/images/brique.jpg"));
+		      //TODO PLACER L'imagE 
+		      int var1=50 ; 
+		      int var2=100; 
+		      
+	       for(int i=0 ;i < 10 ; i++){
+	    	   for(int j=0 ; j<10 ; j++){
+	    		   g2.setColor(Color.white);
+	    		   
+	    		   g2.drawImage(img2, var1, var2, 30, 30, this);
+	    		   
+	    		   g2.drawRect(var1, var2, 30, 30);
+	    		   var1=var1+30; 
+	    		   var2=var2+30;
+	    	   }
+	    	   
+	    	   
+	    	   
+	       }
+		      
+		      
+		      
+		     
+
+		      
+		      
+		      
+		      
+		      
+
+
 		      
 		    
 		 
