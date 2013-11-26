69d6c33606e77569fb6fc7b72307ceaa55a8d3d0
Belynda <Belynda@Belynda-PC>
2013-11-26 14:34:15 +0100

    Ajout des nouvelles ressources

---
+++ Projet IK3/src/Test.java
@@ -0,0 +1,17 @@
+
+  
+public class Test {
+	
+	static Plateau jeu= new Plateau("src/file.txt");
+	  int i=0,j=0;
+  public static void main(String[] args){
+ Fenetre fen = new Fenetre() ; 
+ // Plateau jeu= new Plateau("src/file.txt");
+
+     System.out.println("taile:"+jeu.tableauDeBlock.size());
+     jeu.affichePlateau();
+   
+
+ 
+  }       
+}
